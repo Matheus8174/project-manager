f652a2abbeb47860d8601bd510863cb77cfd9550
Matheus8174 <souzamatheus8174@gmail.com>
2021-03-30 19:55:55 -0300

    feat: create session service

---
+++ src/service/SessionService.ts
@@ -0,0 +1,51 @@
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import AppError from '../errors/AppError';
+import iUserRepository from '../interfaces/iUserRepository';
+import User from '../models/User';
+import UserRepository from '../repositories/UserRepository';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  token: string;
+  user: User;
+}
+
+class SessionService {
+  private userRepository: iUserRepository;
+
+  constructor(userRepository: UserRepository) {
+    this.userRepository = userRepository;
+  }
+
+  public async execute({ email, password }: Request): Promise<Response> {
+    const user = await this.userRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('Credenciais inválidas', 401);
+    }
+
+    const passwordCompare = await compare(password, user.password);
+
+    if (!passwordCompare) {
+      throw new AppError('Credenciais inválidas', 401);
+    }
+
+    const token = sign({}, process.env.APP_SECRET as string, {
+      expiresIn: '1d'
+    });
+
+    delete user.password;
+
+    return {
+      token,
+      user
+    };
+  }
+}
+
+export default SessionService;
