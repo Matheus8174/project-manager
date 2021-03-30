c008be8bb5b6ddea0e656d4e08f0215e1af89c73
Matheus8174 <souzamatheus8174@gmail.com>
2021-03-30 19:55:10 -0300

    refactor: erase comment on index route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,11 @@
 import { Router } from 'express';
 import userRoutes from './user';
+import sessionRoutes from './session';
 
 const routes = Router();
 const prefixRoutes = '/api/v1';
 
-// routes.get('/', (request, response) => {
-//   return response.json({ message: 'hello' });
-// });
-
 routes.use(`${prefixRoutes}/users`, userRoutes);
+routes.use(`${prefixRoutes}/sessions`, sessionRoutes);
 
 export default routes;
