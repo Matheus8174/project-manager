b0ba0ce97bc66f05e81dd6bcd69bbbb7293392e7
Matheus8174 <souzamatheus8174@gmail.com>
2021-03-30 19:53:24 -0300

    refactor: update server add env configuration and errors logic

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
+import 'express-async-errors';
 import 'reflect-metadata';
+import './config/env';
 import './database/connection';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import routes from './routes';
+import AppError from './errors/AppError';
 
 const port = process.env.PORT || 3000;
 
@@ -11,5 +14,17 @@ app.use(express.json({ limit: '100kb' }));
 app.disable('x-powered-by');
 
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response
+      .status(err.statusCode)
+      .json({ status: 'error', message: err.message });
+  }
+
+  return response
+    .status(500)
+    .json({ status: 'error', message: 'Internal server error' });
+});
 
 app.listen(port);
