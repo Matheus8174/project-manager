dd521ff9b1aa98b2b646e8e72c389d0110793a62
Matheus8174 <souzamatheus8174@gmail.com>
2021-04-19 16:25:19 -0300

    refactor: update iUserRepository add findById and save methods

--- src/interfaces/iUserRepository.ts
+++ src/interfaces/iUserRepository.ts
@@ -3,7 +3,11 @@ import User from '../models/User';
 
 interface iUserRepository {
   findByEmail(email: string): Promise<User | undefined>;
+  findById(id: string): Promise<User | undefined>;
   create(createUserDTO: CreateUserDTO): Promise<User>;
+
+  //metodo de atualizar
+  save(user: User): Promise<User>;
 }
 
 export default iUserRepository;
